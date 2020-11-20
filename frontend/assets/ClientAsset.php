dc78e740957ee8b17a9a37278bd6ec5b3cd5b5ec
HomeCrmDvs <dmitryukv@yandex.ru>
2020-11-20 09:14:34 +0300

    antiCash css and js

--- frontend/assets/ClientAsset.php
+++ frontend/assets/ClientAsset.php
@@ -9,8 +9,8 @@ class ClientAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $js = [
-        'js/client.js',
-        'js/datepicker.js',
+        'js/client.js?1',
+        'js/datepicker.js?1',
     ];
     public $depends = [
         'yii\web\JqueryAsset',
